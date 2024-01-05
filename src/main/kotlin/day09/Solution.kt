a8d61b98b3ec56126eb2b2fab877ad959003011e
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-01-05 13:53:16 +0100

    Update Solution.kt

--- src/main/kotlin/day09/Solution.kt
+++ src/main/kotlin/day09/Solution.kt
@@ -1,7 +1,8 @@
 package day09
 
-fun getInput(filePath: String): List<List<Int>> = java.io.File(ClassLoader.getSystemResource(filePath).file).readLines()
-    .map { it.split(" ").map(String::toInt) }
+import java.io.File
+fun getInput(filePath: String): List<List<Int>> =
+    File(ClassLoader.getSystemResource(filePath).file).readLines().map { it.split(" ").map(String::toInt) }
 
 fun List<Int>.extrapolateNext(): Int =
     if (this.all { it == 0 }) 0 else this.last() + this.zipWithNext { a, b -> b - a }.extrapolateNext()
