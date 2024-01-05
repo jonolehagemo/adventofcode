b0476a91445c419f0a2fa7f8b25a1cf293ab5e0a
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-01-05 09:41:44 +0100

    Update Solution.kt

--- src/main/kotlin/day09/Solution.kt
+++ src/main/kotlin/day09/Solution.kt
@@ -2,14 +2,12 @@ package day09
 
 import java.io.File
 
-fun getInput(filePath: String): List<List<Int>> =
-    File(ClassLoader.getSystemResource(filePath).file)
+fun getInput(filePath: String): List<List<Int>> = File(ClassLoader.getSystemResource(filePath).file)
     .readLines()
     .map { it.split(" ").map(String::toInt) }
 
 fun List<Int>.extrapolateNext(): Int =
-    if (this.all { it == 0 }) 0
-    else this.last() + this.zipWithNext { a, b -> b - a }.extrapolateNext()
+    if (this.all { it == 0 }) 0 else this.last() + this.zipWithNext { a, b -> b - a }.extrapolateNext()
 
 fun main() {
     println(getInput("Day09Input.txt").sumOf { it.extrapolateNext() })
