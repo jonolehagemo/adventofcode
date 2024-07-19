46502b1c6f96ef8cdb475a7ab869d38cb2f5dd98
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-07-19 08:16:24 +0200

    Pack day 10

--- src/main/kotlin/day10/Solution.kt
+++ src/main/kotlin/day10/Solution.kt
@@ -1,7 +1,5 @@
 package day10
 
-
-
 import datastructures.Coordinate
 import datastructures.Grid
 import extensions.filePathToGrid
@@ -18,36 +16,25 @@ fun tileToDirection(tile: Char): List<Pair<Int, Int>> = when (tile) {
     else -> emptyList()
 }
 
-fun findLoop(grid: Grid, start: Coordinate): List<Coordinate> = dfs(Coordinate(-1, -1), start, grid, emptyList())
-
 tailrec fun dfs(previous: Coordinate, current: Coordinate, grid: Grid, visited: List<Coordinate>): List<Coordinate> {
     val currentTile = grid.tile(current)
     val next = tileToDirection(currentTile).firstNotNullOf { direction ->
-        val nextCoordinate = Coordinate(
-            row = current.row + direction.first,
-            column = current.column + direction.second
-        )
-        if (nextCoordinate.row != previous.row || nextCoordinate.column != previous.column)
-            nextCoordinate
+        val nextCoordinate = Coordinate(row = current.row + direction.first, column = current.column + direction.second)
+        if (nextCoordinate.row != previous.row || nextCoordinate.column != previous.column) nextCoordinate
         else null
     }
-
-    return if (currentTile == 'S' && visited.isNotEmpty()) {
-        return visited
-    } else dfs(current, next, grid, visited.plus(current))
+    return if (currentTile == 'S' && visited.isNotEmpty()) return visited
+    else dfs(current, next, grid, visited.plus(current))
 }
 
 fun main() {
     val grid = "Day10Input.txt".filePathToGrid('.')
     val start = grid.findCoordinateByTile('S').first()
-    val loop = findLoop(grid, start)
+    val loop = dfs(Coordinate(-1, -1), start, grid, emptyList())
     println("Task 1: ${loop.size / 2}")
-
     // https://en.wikipedia.org/wiki/Pick%27s_theorem
     val area = loop
-        .zip(loop.drop(1) + loop.take(1)) { a, b ->
-            a.column * b.row - a.row * b.column
-        }
+        .zip(loop.drop(1) + loop.take(1)) { a, b -> a.column * b.row - a.row * b.column }
         .sum()
         .absoluteValue / 2
     println("Task 2: ${area - loop.size / 2 + 1}")
