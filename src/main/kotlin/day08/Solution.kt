9b2f104074c4f6ecfe7d73047a8776ea3f8835ea
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-07-22 13:50:21 +0200

    Simplify day 8

--- src/main/kotlin/day08/Solution.kt
+++ src/main/kotlin/day08/Solution.kt
@@ -1,19 +1,18 @@
 package day08
 
 import extensions.filePathToStringList
+import extensions.println
 
 fun getInstructions(filePath: String): List<Int> =
     filePath.filePathToStringList().first().map { if (it == 'L') 0 else 1 }
 
-fun getNetwork(filePath: String): Map<String, List<String>> {
-    val regex = Regex("(\\w+) = \\((\\w+), (\\w+)\\)")
-    return filePath.filePathToStringList()
+fun getNetwork(filePath: String): Map<String, List<String>> =
+    filePath.filePathToStringList()
         .drop(2)
         .associate {
-            val (key, left, right) = regex.find(it)!!.destructured
+            val (key, left, right) = Regex("(\\w+) = \\((\\w+), (\\w+)\\)").find(it)!!.destructured
             return@associate key to listOf(left, right)
         }
-}
 
 fun countSteps(instructions: List<Int>, network: Map<String, List<String>>, start: String): Int {
     var steps = 0
@@ -27,29 +26,19 @@ fun countSteps(instructions: List<Int>, network: Map<String, List<String>>, star
     return steps
 }
 
-fun findGreatestCommonDivisor(number1: Long, number2: Long): Long {
-    var num1 = number1
-    var num2 = number2
-
-    while (num2 != 0L) {
-        val temp = num2
-        num2 = num1 % num2
-        num1 = temp
-    }
-
-    return num1
-}
+fun findGreatestCommonDivisor(a: Long, b: Long): Long = if (b == 0L) a else findGreatestCommonDivisor(b, a % b)
 
 fun main() {
     val instructions = getInstructions("Day08Input.txt")
     val network = getNetwork("Day08Input.txt")
     // task1
-    println(countSteps(instructions, network, "AAA"))
+    countSteps(instructions, network, "AAA")
+        .println()
 
     //task 2
     network.keys
         .filter { it.endsWith('A') }
         .map { countSteps(instructions, network, it).toLong() }
         .fold(1L) { sum, acc -> sum * acc / findGreatestCommonDivisor(sum, acc) }
-        .also { println(it) }
+        .println()
 }
