f6869afec7660ff57626eb0a8f914d5e6864d401
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-07-26 10:39:13 +0200

    Day 1 fix bug

--- src/main/kotlin/day01/Solution.kt
+++ src/main/kotlin/day01/Solution.kt
@@ -8,9 +8,9 @@ fun String.deStringify(): String =
     listOf("one o1e", "two t2o", "three t3e", "four f4r", "five f5e", "six s6x", "seven s7n", "eight e8t", "nine n9e")
         .fold(this) { result, s -> result.replace(s.split(" ")[0], s.split(" ")[1]) }
 
-fun String.toInt(): Int = ("" + first { it.isDigit() } + last { it.isDigit() }).toInt()
+fun String.toInteger(): Int = ("" + first { it.isDigit() } + last { it.isDigit() }).toInt()
 
 fun main() {
-    "Day01Input.txt".filePathToStringList().sumOf { it.toInt() }.println()
-    "Day01Input.txt".filePathToStringList().sumOf { it.deStringify().toInt() }.println()
+    "Day01Input.txt".filePathToStringList().sumOf { it.toInteger() }.println()
+    "Day01Input.txt".filePathToStringList().sumOf { it.deStringify().toInteger() }.println()
 }
--- src/test/kotlin/day01/SolutionSpec.kt
+++ src/test/kotlin/day01/SolutionSpec.kt
@@ -9,23 +9,11 @@ import io.kotest.matchers.shouldBe
 class SolutionSpec : BehaviorSpec({
     Given("processing of test input") {
         forAll(
-            row("Day01TestInput1.txt", emptyMap(), 142),
-            row(
-                "Day01TestInput2.txt", mapOf(
-                    "one" to '1',
-                    "two" to '2',
-                    "three" to '3',
-                    "four" to '4',
-                    "five" to '5',
-                    "six" to '6',
-                    "seven" to '7',
-                    "eight" to '8',
-                    "nine" to '9'
-                ), 281
-            ),
-        ) { filepath, mapping, expected ->
+            row("Day01TestInput1.txt",142),
+            row("Day01TestInput2.txt", 281),
+        ) { filepath, expected ->
             Then(filepath) {
-                filepath.filePathToStringList().sumOf { it.toInt(mapping) } shouldBe expected
+                filepath.filePathToStringList().sumOf { it.deStringify().toInteger() } shouldBe expected
             }
         }
     }
