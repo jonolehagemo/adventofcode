9ac3726f9b70ffeeb3417c4951935dafafd5e8fa
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-08-03 10:22:45 +0200

    Day 18 refactor naming

--- src/main/kotlin/day18/Solution.kt
+++ src/main/kotlin/day18/Solution.kt
@@ -5,7 +5,6 @@ import extensions.filePathToStringList
 import extensions.println
 import kotlin.math.abs
 import kotlin.math.absoluteValue
-import kotlin.math.floor
 
 fun List<String>.toCoordinates1(): List<Coordinate> = map {
         val (direction , length) = it.split(" ")
@@ -32,11 +31,11 @@ fun String.toDirection(): Coordinate = when (this) {
 }
 
 fun List<Coordinate>.calculateAreaByCircumference(): Long {
-    val area = zip(drop(1) + take(1)) { i, j -> i.column * j.row - i.row * j.column }
-        .sum().absoluteValue / 2
-    val b = zip(drop(1) + take(1)) { i, j -> abs(i.shortestPath(j)) }.sum()
-    val i = area - floor((b / 2).toDouble()).toLong() + 1
-    return i + b
+    // https://en.wikipedia.org/wiki/Pick%27s_theorem
+    val area = zip(drop(1) + take(1)) { a, b -> a.column * b.row - a.row * b.column }.sum().absoluteValue / 2
+    val boundary = zip(drop(1) + take(1)) { a, b -> abs(a.shortestPath(b)) }.sum()
+    val interior = area - (boundary / 2) + 1
+    return interior + boundary
 }
 
 fun main() {
