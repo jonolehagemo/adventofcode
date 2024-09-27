7afdba8f0394283d74744ec5f635e834b2d8ade7
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-09-27 13:28:19 +0200

    Update day 13

--- src/main/kotlin/aoc2023/day13/Solution.kt
+++ src/main/kotlin/aoc2023/day13/Solution.kt
@@ -4,53 +4,19 @@ import extensions.filePathToListOfStringList
 import extensions.println
 import extensions.transpose
 
-fun List<String>.patternSum(): Int =
-    this.findSplit() * 100 + this.transpose().findSplit()
+fun List<String>.patternSum(difference: Int): Int = sum(difference) * 100 + transpose().sum(difference)
 
-fun List<String>.findSplit(): Int {
-    for (i in 0..this.size - 2) {
-        if (this.reflects(i))
-            return i + 1
-    }
-
-    return 0
-}
-
-fun List<String>.reflects(start: Int): Boolean {
-    var lower = start
-    var upper = start + 1
-    var fixedOne = false
-
-    while (0 <= lower && upper < this.size) {
-        if (this[lower] != this[upper])
-            if (fixedOne)
-                return false
-            else if (isFixable(this[lower], this[upper]))
-                fixedOne = true
-            else
-                return false
-
-        lower--
-        upper++
-    }
-
-    return true
-}
-
-fun isFixable(row1: String, row2: String): Boolean {
-    var differences = 0
-
-    for ((a, b) in row1 zip row2)
-        if (a != b)
-            differences++
-
-    return differences == 1
-}
-
+fun List<String>.sum(expectedDifference: Int): Int =
+    (1..<size)
+        .map { index ->
+            index to subList(0, index).reversed().joinToString("|")
+                .zip(subList(index, size).joinToString("|"))
+                .count { (a, b) -> a != b }
+        }
+        .firstOrNull { (_, actualDifference) -> actualDifference == expectedDifference }
+        ?.first ?: 0
 
 fun main() {
-    "aoc2023/Day13Input.txt".filePathToListOfStringList()
-        .sumOf { it.patternSum() }
-        .println()
-    // 26473 is too low
+    "aoc2023/Day13Input.txt".filePathToListOfStringList().sumOf { it.patternSum(0) }.println()
+    "aoc2023/Day13Input.txt".filePathToListOfStringList().sumOf { it.patternSum(1) }.println()
 }
--- src/test/kotlin/aoc2023/day13/SolutionSpec.kt
+++ src/test/kotlin/aoc2023/day13/SolutionSpec.kt
@@ -7,17 +7,52 @@ import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class SolutionSpec : BehaviorSpec({
+    Given("test patterns") {
+        val pattern1 = """#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.""".trimIndent()
+
+        val pattern2 = """#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#""".trimIndent()
+
+        forAll(
+            row("pattern1", pattern1, 0, 5),
+            row("pattern2", pattern2, 0, 400),
+            row("pattern1", pattern1, 1, 300),
+            row("pattern2", pattern2, 1, 100),
+        ) { description, pattern, difference, expectedSum
+            ->
+            When("$description: diff = $difference -> $expectedSum") {
+                val result = pattern.split("\n").patternSum(difference)
+
+                Then("the resultSum result should be as expected") {
+                    result shouldBe expectedSum
+                }
+            }
+        }
+    }
+
     Given("file paths to test input files") {
         forAll(
-            row("aoc2023/Day13TestInput1.txt", 405),
-        ) { filePath, expectedSum
+            row("aoc2023/Day13TestInput1.txt", 0, 405),
+            row("aoc2023/Day13Input.txt", 0, 39939),
+        ) { filePath, difference, expectedSum
             ->
             When(filePath) {
                 val input = filePath.filePathToListOfStringList()
 
                 val result = input
                     .sumOf {
-                        it.patternSum()
+                        it.patternSum(difference)
                     }
 
                 Then("the resultSum result should be as expected") {
@@ -27,7 +62,18 @@ class SolutionSpec : BehaviorSpec({
         }
     }
 
+
 })
+
+/*
+#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+*/
 
 /*
 #.##..##.
