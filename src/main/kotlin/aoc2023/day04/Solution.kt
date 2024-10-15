85bd6641e8bcf67c912bfe95d1672cea5d88b2ef
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-10-15 10:43:27 +0200

    aoc 2023 day 4 Use take

--- src/main/kotlin/aoc2023/day04/Solution.kt
+++ src/main/kotlin/aoc2023/day04/Solution.kt
@@ -9,7 +9,7 @@ fun String.toSet(): Set<String> = trim().split("\\s+".toRegex()).toSet()
 fun List<String>.toWinningNumbersCount(): List<Int> =
     map { it.substringAfter(":").split("|").let { (w, n) -> (w.toSet() intersect n.toSet()).size } }
 
-fun List<Int>.sumCopies(): Int = reversed().fold(listOf(0)) { r, i -> listOf(1 + r.subList(0, i).sum()) + r }.sum()
+fun List<Int>.sumCopies(): Int = reversed().fold(listOf(0)) { r, i -> listOf(1 + r.take(i).sum()) + r }.sum()
 
 fun main() {
     val wins = "aoc2023/Day04Input.txt".filePathToStringList().toWinningNumbersCount()
