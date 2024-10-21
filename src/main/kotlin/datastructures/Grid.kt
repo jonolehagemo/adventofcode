106a4afbe0bf7e66b06c794da1dee7fa8c3e0a77
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-10-21 21:48:49 +0200

    Update Grid.kt

--- src/main/kotlin/datastructures/Grid.kt
+++ src/main/kotlin/datastructures/Grid.kt
@@ -3,110 +3,162 @@ package datastructures
 data class Grid(
     val coordinateCharMap: Map<Coordinate, Char>,
     val defaultValue: Char = ' ',
-    val start: Coordinate = coordinateCharMap
-        .entries
-        .first { it.value != defaultValue }
-        .key,
-    val finish: Coordinate = coordinateCharMap
-        .entries
-        .last { it.value != defaultValue }
-        .key,
+    val start: Coordinate =
+        coordinateCharMap
+            .entries
+            .first { it.value != defaultValue }
+            .key,
+    val finish: Coordinate =
+        coordinateCharMap
+            .entries
+            .last { it.value != defaultValue }
+            .key,
 ) {
-    fun rowRange(): LongRange = 0..coordinateCharMap.keys.maxOf { it.row }
-    fun columnRange(): LongRange = 0..coordinateCharMap.keys.maxOf { it.column }
+    fun rowMax(): Long = coordinateCharMap.keys.maxOf { it.row }
+
+    fun columnMax(): Long = coordinateCharMap.keys.maxOf { it.column }
+
+    fun rowRange(): LongRange = 0..rowMax()
+
+    fun columnRange(): LongRange = 0..columnMax()
 
     fun isInBounds(c: Coordinate): Boolean = c.row in rowRange() && c.column in columnRange()
 
     fun isOutOfBounds(c: Coordinate): Boolean = !isInBounds(c)
 
-    fun findCoordinateByTile(tile: Char): List<Coordinate> = coordinateCharMap
-        .toList()
-        .filter { it.second == tile }
-        .map { it.first }
+    fun findCoordinateByTile(tile: Char): List<Coordinate> =
+        coordinateCharMap
+            .toList()
+            .filter { it.second == tile }
+            .map { it.first }
 
     fun tile(c: Coordinate): Char = coordinateCharMap.getOrDefault(c, defaultValue)
+
     private fun tileCount(c: Coordinate): Int = if (tile(c) != defaultValue) 1 else 0
 
-    fun neighboursCount(c: Coordinate): Int =
-        if (tile(c) == defaultValue) 0 else c.neighboursNEWS().sumOf { tileCount(it) }
+    fun neighboursCount(c: Coordinate): Int = if (tile(c) == defaultValue) 0 else c.neighboursNEWS().sumOf { tileCount(it) }
 
-    fun neighbours(c: Coordinate): List<Coordinate> = when (tile(c)) {
-        '^' -> listOf(c.north())
-        'v' -> listOf(c.south())
-        '>' -> listOf(c.east())
-        '<' -> listOf(c.west())
-        '.' -> {
-            val mutableList: MutableList<Coordinate> = mutableListOf()
-            if (tile(c.north()) !in setOf(defaultValue, 'v')) mutableList.add(c.north())
-            if (tile(c.south()) !in setOf(defaultValue, '^')) mutableList.add(c.south())
-            if (tile(c.east()) !in setOf(defaultValue, '<')) mutableList.add(c.east())
-            if (tile(c.west()) !in setOf(defaultValue, '>')) mutableList.add(c.west())
+    fun neighbours(c: Coordinate): List<Coordinate> =
+        when (tile(c)) {
+            '^' -> listOf(c.north())
+            'v' -> listOf(c.south())
+            '>' -> listOf(c.east())
+            '<' -> listOf(c.west())
+            '.' -> {
+                val mutableList: MutableList<Coordinate> = mutableListOf()
+                if (tile(c.north()) !in setOf(defaultValue, 'v')) mutableList.add(c.north())
+                if (tile(c.south()) !in setOf(defaultValue, '^')) mutableList.add(c.south())
+                if (tile(c.east()) !in setOf(defaultValue, '<')) mutableList.add(c.east())
+                if (tile(c.west()) !in setOf(defaultValue, '>')) mutableList.add(c.west())
 
-            mutableList
+                mutableList
+            }
+
+            else -> emptyList()
         }
 
-        else -> emptyList()
-    }
+    fun sliceNorth(c: Coordinate): List<Pair<Coordinate, Char>> =
+        (c.north().row downTo 0L)
+            .map { row -> c.copy(row = row) to tile(c.copy(row = row)) }
 
-    fun nodes(): List<Coordinate> = coordinateCharMap
-        .keys
-        .map { it to neighboursCount(it) }
-        .filter { (_, count) -> count != 2 }
-        .map { it.first }
+    fun sliceEast(c: Coordinate): List<Pair<Coordinate, Char>> =
+        (c.east().column..coordinateCharMap.keys.maxOf { it.column })
+            .map { column -> c.copy(column = column) to tile(c.copy(column = column)) }
 
-    private fun edge(previous: Coordinate, current: Coordinate, steps: Int = 0): Pair<Coordinate, Int> = when {
-        previous == finish ->
-            previous to 0
+    fun sliceSouth(c: Coordinate): List<Pair<Coordinate, Char>> =
+        (c.south().row..coordinateCharMap.keys.maxOf { it.row })
+            .map { row -> c.copy(row = row) to tile(c.copy(row = row)) }
 
-        neighboursCount(current) == 2 ->
-            neighbours(current)
-                .firstOrNull { next -> next != previous }
-                .let { next -> edge(current, next ?: previous, steps + 1) }
+    fun sliceWest(c: Coordinate): List<Pair<Coordinate, Char>> =
+        (c.west().column downTo 0)
+            .map { column -> c.copy(column = column) to tile(c.copy(column = column)) }
 
-        else ->
-            current to steps
-    }
-
-    fun toGraph() =
-        Graph(nodes().associateWith { start ->
-            neighbours(start).map { neighbour -> edge(start, neighbour, 1) }.toSet()
-        })
-
-    override fun toString(): String = this
-        .rowRange().joinToString("\n") { rowIndex ->
-            columnRange().joinToString("") { columnIndex ->
-                tile(
-                    Coordinate(
-                        row = rowIndex,
-                        column = columnIndex
-                    )
-                ).toString()
+    fun export(): List<Pair<Coordinate, Char>> =
+        rowRange().flatMap { row ->
+            columnRange().map { column ->
+                Coordinate(row, column) to tile(Coordinate(row, column))
             }
         }
 
-    fun transpose(): Grid = Grid(
-        this.coordinateCharMap.entries.associate { Coordinate(it.key.column, it.key.row) to it.value },
-        defaultValue
-    )
+    fun nodes(): List<Coordinate> =
+        coordinateCharMap
+            .keys
+            .map { it to neighboursCount(it) }
+            .filter { (_, count) -> count != 2 }
+            .map { it.first }
 
+    private fun edge(
+        previous: Coordinate,
+        current: Coordinate,
+        steps: Int = 0,
+    ): Pair<Coordinate, Int> =
+        when {
+            previous == finish ->
+                previous to 0
+
+            neighboursCount(current) == 2 ->
+                neighbours(current)
+                    .firstOrNull { next -> next != previous }
+                    .let { next -> edge(current, next ?: previous, steps + 1) }
+
+            else ->
+                current to steps
+        }
+
+    fun toGraph() =
+        Graph(
+            nodes().associateWith { start ->
+                neighbours(start).map { neighbour -> edge(start, neighbour, 1) }.toSet()
+            },
+        )
+
+    override fun toString(): String =
+        this
+            .rowRange()
+            .joinToString("\n") { rowIndex ->
+                columnRange().joinToString("") { columnIndex ->
+                    tile(
+                        Coordinate(
+                            row = rowIndex,
+                            column = columnIndex,
+                        ),
+                    ).toString()
+                }
+            }
+
+    fun transpose(): Grid =
+        Grid(
+            this.coordinateCharMap.entries.associate { Coordinate(it.key.column, it.key.row) to it.value },
+            defaultValue,
+        )
 
     fun countDistinctRowTiles(): List<IndexedValue<Int>> =
-        this.rowRange()
+        this
+            .rowRange()
             .map { rowIndex ->
-                IndexedValue(index = rowIndex.toInt(), value = this.columnRange()
-                    .map { columnIndex -> tile(Coordinate(rowIndex, columnIndex)) }
-                    .distinct()
-                    .count()
+                IndexedValue(
+                    index = rowIndex.toInt(),
+                    value =
+                        this
+                            .columnRange()
+                            .map { columnIndex -> tile(Coordinate(rowIndex, columnIndex)) }
+                            .distinct()
+                            .count(),
                 )
             }
 
     fun countDistinctColumnTiles(): List<IndexedValue<Int>> =
-        this.columnRange()
+        this
+            .columnRange()
             .map { columnIndex ->
-                IndexedValue(index = columnIndex.toInt(), value = this.rowRange()
-                    .map { rowIndex -> tile(Coordinate(rowIndex, columnIndex)) }
-                    .distinct()
-                    .count()
+                IndexedValue(
+                    index = columnIndex.toInt(),
+                    value =
+                        this
+                            .rowRange()
+                            .map { rowIndex -> tile(Coordinate(rowIndex, columnIndex)) }
+                            .distinct()
+                            .count(),
                 )
             }
 }
