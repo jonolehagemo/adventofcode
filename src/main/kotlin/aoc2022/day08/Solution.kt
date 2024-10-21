0bf779f67baf7fbf9304a8cb67edb61e2b113cb8
Jon Ole Hagemo <jonolehagemo@users.noreply.github.com>
2024-10-21 21:49:02 +0200

    Add aoc 2022 day 8

---
+++ src/main/kotlin/aoc2022/day08/Solution.kt
@@ -0,0 +1,46 @@
+package aoc2022.day08
+
+import datastructures.Coordinate
+import datastructures.Grid
+import extensions.filePathToStringList
+import extensions.println
+import extensions.toGrid
+
+fun List<Pair<Coordinate, Char>>.findHighestTree(): Int = maxOfOrNull { it.second.toString().toInt() } ?: -1
+
+fun Grid.countVisibleTrees(): Int =
+    export()
+        .map { (c, char) -> c to char.toString().toInt() }
+        .map { (c, value) ->
+            c.row == 0L ||
+                c.row == rowMax() ||
+                c.column == 0L ||
+                c.column == columnMax() ||
+                sliceNorth(c).findHighestTree() < value ||
+                sliceEast(c).findHighestTree() < value ||
+                sliceWest(c).findHighestTree() < value ||
+                sliceSouth(c).findHighestTree() < value
+        }.count { it }
+
+fun List<Pair<Coordinate, Char>>.findScenicView(value: Int): Int =
+    this
+        .map { (_, v) -> v.toString().toInt() }
+        .withIndex()
+        .firstOrNull { value <= it.value }
+        ?.index
+        ?.plus(1) ?: this.size
+
+fun Grid.maxScenicView(): Int =
+    export()
+        .map { (c, char) -> c to char.toString().toInt() }
+        .maxOf { (c, value) ->
+            listOf(sliceNorth(c), sliceEast(c), sliceSouth(c), sliceWest(c))
+                .map { it.findScenicView(value) }
+                .reduce { a, b -> a * b }
+        }
+
+fun main() {
+    val input = "aoc2022/Day08Input.txt".filePathToStringList().toGrid('0')
+    input.countVisibleTrees().println()
+    input.maxScenicView().println()
+}
---
+++ src/test/kotlin/aoc2022/day08/SolutionSpec.kt
@@ -0,0 +1,47 @@
+package aoc2022.day08
+
+import extensions.filePathToStringList
+import extensions.toGrid
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class SolutionSpec :
+    BehaviorSpec({
+        Given("counting visible trees, task 1") {
+            forAll(
+                row("aoc2022/Day08TestInput1.txt", 21),
+//                row("aoc2022/Day08Input.txt", 1854),
+            ) { filePath, expectedSize ->
+                When("$filePath -[size]-> $expectedSize") {
+                    val input =
+                        filePath.filePathToStringList().toGrid('0')
+                    val result =
+                        input.countVisibleTrees()
+
+                    Then("$result -> $expectedSize") {
+                        result shouldBe expectedSize
+                    }
+                }
+            }
+        }
+
+        Given("counting visible trees, task 2") {
+            forAll(
+                row("aoc2022/Day08TestInput1.txt", 8),
+//                row("aoc2022/Day08Input.txt", 527340),
+            ) { filePath, expectedSize ->
+                When("$filePath -[size]-> $expectedSize") {
+                    val input =
+                        filePath.filePathToStringList().toGrid('0')
+                    val result =
+                        input.maxScenicView()
+
+                    Then("$result -> $expectedSize") {
+                        result shouldBe expectedSize
+                    }
+                }
+            }
+        }
+    })
